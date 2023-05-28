ca408eb443c545c20ababedaa474e77b5e92fa15
Artur <golubev.artur.1986@gmail.com>
2023-05-28 14:40:40 +0300

    feat: add basic page portfolio.html

--- artur-front-end/src/js/files/script.js
+++ artur-front-end/src/js/files/script.js
@@ -51,15 +51,22 @@ const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee__content");
 if (marqueeContent) {
-
-
 	root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-
 	for (let i = 0; i < marqueeElementsDisplayed; i++) {
 		marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 	}
 }
+//========================================================================================================================================================
+const goTopBtn = document.querySelector('[data-go-top]');
+window.addEventListener('scroll', function () {
+	if (window.scrollY >= 120) {
 
+		goTopBtn.classList.add('active');
+	} else {
+
+		goTopBtn.classList.remove('active');
+	}
+});
 
 
 

4c7315deacb1c7fef434396c5b6ea2488f390e4e
Artur <golubev.artur.1986@gmail.com>
2023-06-25 16:17:05 +0300

    fix: lazyload

--- artur-front-end/src/js/files/functions.js
+++ artur-front-end/src/js/files/functions.js
@@ -227,6 +227,58 @@ export function filterGallery() {
 	}
 }
 
+// Модуль "Сustom сursor" =======================================================================================================================================================================================================================
+export function customCursor(isShadowTrue) {
+	const wrapper = document.querySelector('[data-custom-cursor]') ? document.querySelector('[data-custom-cursor]') : document.documentElement;
+	if (wrapper && !isMobile.any()) {
+		// Створюємо та додаємо об'єкт курсору
+		const cursor = document.createElement('div');
+		cursor.classList.add('fls-cursor');
+		cursor.style.opacity = 0;
+		cursor.insertAdjacentHTML('beforeend', `<span class="fls-cursor__pointer"></span>`);
+		isShadowTrue ? cursor.insertAdjacentHTML('beforeend', `<span class="fls-cursor__shadow"></span>`) : null;
+		wrapper.append(cursor);
+
+		const cursorPointer = document.querySelector('.fls-cursor__pointer');
+		const cursorPointerStyle = {
+			width: cursorPointer.offsetWidth,
+			height: cursorPointer.offsetHeight
+		}
+		let cursorShadow, cursorShadowStyle;
+		if (isShadowTrue) {
+			cursorShadow = document.querySelector('.fls-cursor__shadow');
+			cursorShadowStyle = {
+				width: cursorShadow.offsetWidth,
+				height: cursorShadow.offsetHeight
+			}
+		}
+		function mouseActions(e) {
+			if (e.type === 'mouseout') {
+				cursor.style.opacity = 0;
+			} else if (e.type === 'mousemove') {
+				cursor.style.removeProperty('opacity');
+				if (e.target.closest('button') || e.target.closest('a') || e.target.closest('input') || (window.getComputedStyle(e.target).cursor !== 'none' && window.getComputedStyle(e.target).cursor !== 'default')) {
+					cursor.classList.add('_hover');
+				} else {
+					cursor.classList.remove('_hover');
+				}
+			} else if (e.type === 'mousedown') {
+				cursor.classList.add('_active');
+
+			} else if (e.type === 'mouseup') {
+				cursor.classList.remove('_active');
+			}
+			cursorPointer ? cursorPointer.style.transform = `translate3d(${e.clientX - cursorPointerStyle.width / 2}px, ${e.clientY - cursorPointerStyle.height / 2}px, 0)` : null;
+			cursorShadow ? cursorShadow.style.transform = `translate3d(${e.clientX - cursorShadowStyle.width / 2}px, ${e.clientY - cursorShadowStyle.height / 2}px, 0)` : null;
+		}
+
+		window.addEventListener('mouseup', mouseActions);
+		window.addEventListener('mousedown', mouseActions);
+		window.addEventListener('mousemove', mouseActions);
+		window.addEventListener('mouseout', mouseActions);
+	}
+}
+
 // Модуль "показать еще" =======================================================================================================================================================================================================================
 export function showMore() {
 	const btnshowMoreCards = document.querySelector('.button-load');
